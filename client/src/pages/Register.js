658922f9ea4ecb776b0bf5e833d769bfba4e3cf9
BekCodingAddict <experte001@gmail.com>
2023-09-24 07:40:17 +0900

    Some Changes!

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -4,16 +4,17 @@ import { Form } from 'antd';
 function Register() {
     return (
         <div className='h-screen d-flex justify-content-center align-items-center'>
-            <div className='w-300 '>
+            <div className='w-400 card p-3'>
+            <h1 className='text-lg'>Express Bus - Register</h1><hr/>
                 <Form layout='vertical'>
                     <Form.Item label='Name'>
-                        <input type='text' />
+                        <input type='text'></input>
                     </Form.Item>
                     <Form.Item label='Email'>
-                        <input type='text' />
+                        <input type='text'></input>
                     </Form.Item>
                     <Form.Item label='Password'>
-                        <input type="password" />
+                        <input type="password"></input>
                     </Form.Item>
                 </Form>
             </div>
