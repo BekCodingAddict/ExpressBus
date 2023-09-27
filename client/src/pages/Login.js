30235a4c8064c221dbb0123ebd43c8691bd809c9
Otabek Sotvoldiev <70604119+BekCodingAddict@users.noreply.github.com>
2023-09-27 16:44:56 +0900

    Login Page Form has complated!

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React from 'react';
+import { Form } from 'antd';
+import { Link } from "react-router-dom";
 
-function Login(){
+function Login() {
+
+    const onFinish = (values) => {
+        console.log(values);
+    }
     return (
-        <div>Login</div>
+        <div className='h-screen d-flex justify-content-center align-items-center'>
+            <div className='w-400 card p-3'>
+                <h1 className='text-lg'>Express Bus - Register</h1><hr />
+
+                <Form layout='vertical' onFinish={onFinish}>
+
+                    <Form.Item label='Email' name='email'>
+                        <input type='text'></input>
+                    </Form.Item>
+                    <Form.Item label='Password' name='password'>
+                        <input type="password"></input>
+                    </Form.Item>
+
+                    <div className='d-flex justify-content-between align-items-center'>
+                        <Link to="/register">Click here to Register</Link>
+                        <button className='secondary-btn' type="submit">Login</button>
+                    </div>
+
+                </Form>
+            </div>
+        </div>
     )
 }
 
