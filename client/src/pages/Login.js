fbfab2b0190623604d2c243bd5e10e01ab7e6663
Otabek Sotvoldiev <70604119+BekCodingAddict@users.noreply.github.com>
2023-09-27 16:48:00 +0900

    Login Form has complated!

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -10,7 +10,7 @@ function Login() {
     return (
         <div className='h-screen d-flex justify-content-center align-items-center'>
             <div className='w-400 card p-3'>
-                <h1 className='text-lg'>Express Bus - Register</h1><hr />
+                <h1 className='text-lg'>Express Bus - Login</h1><hr />
 
                 <Form layout='vertical' onFinish={onFinish}>
 
