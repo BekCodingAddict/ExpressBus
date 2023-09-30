6fa362bd74fa0bced7957191c4009a5851f2d626
BekCodingAddict <experte001@gmail.com>
2023-09-30 22:17:56 +0900

    Meni Items added!

--- client/src/component/DefaultLayout.js
+++ client/src/component/DefaultLayout.js
@@ -1,10 +1,52 @@
 import React from "react";
+import '../resources/layout.css';
+function DefaultLayout({ children }) {
 
-function DefaultLayout({children}) {
+    const userMenu=[]
+    const adminMenu=[
+        {
+            name:"Home",
+            path: '/admin',
+            icon:'ri-home-line',
+        },
+        {
+            name:'Buses',
+            path:'/admin/buses',
+            icon:'ri-bus-fill',
+        },
+        {
+            name:'Users',
+            path:'/admin/users',
+            icon:'ri-user-line',
+        },
+        {
+            name:'Bookings',
+            path:'/admin/bookings',
+            icon:'ri-file-list-line',
+        },
+        {
+            name:'Logout',
+            path:'/logout',
+            icon:'ri-logout-box-line',
+        }
+    ]
+    const menuToBeRender=adminMenu
     return (
-        <div>
-            <h1>Header</h1>
-            {children}
+        <div className="layout-parent" >
+            <div className="sidebar">
+                {menuToBeRender.map((iten,index)=>{
+                    return <div></div>;
+                })}
+            </div>
+            <div className="body">
+                <div className="header">
+                    header
+                </div>
+                <div className="content">
+                    {children}
+                </div>
+            </div>
+
         </div>
     )
 }
