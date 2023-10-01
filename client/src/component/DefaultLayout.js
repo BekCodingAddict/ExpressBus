a271dc5ab24b5d2163e0771cedb1c5939470078c
BekCodingAddict <experte001@gmail.com>
2023-10-02 01:21:21 +0900

    Layout Responsiveness has finished!

--- client/src/component/DefaultLayout.js
+++ client/src/component/DefaultLayout.js
@@ -1,11 +1,32 @@
 import React from "react";
 import '../resources/layout.css';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function DefaultLayout({ children }) {
     const navigate = useNavigate();
-
-    const userMenu = []
+    const [collapsed, setCollapsed] = React.useState(false);
+    const { user } = useSelector(state => state.users)
+    const userMenu = [
+        {
+            name: 'Home',
+            icon: 'ri-home-line',
+            path: '/'
+        }, {
+            name: 'Booking',
+            icon: 'ri-file-list-line',
+            path: 'bookings'
+        }, {
+            name: 'Profile',
+            icon: 'ri-user-line',
+            path: '/profile'
+        },
+        {
+            name: 'Logout',
+            icon: 'ri-logout-box-line',
+            path: '/logout'
+        }
+    ]
     const adminMenu = [
         {
             name: "Home",
@@ -33,19 +54,36 @@ function DefaultLayout({ children }) {
             icon: 'ri-logout-box-line',
         }
     ]
-    const menuToBeRender = adminMenu;
+    const menuToBeRender = user?.isAdmin ? adminMenu : userMenu;
     const activeRoute = window.location.pathname;
     return (
         <div className="layout-parent" >
             <div className="sidebar">
-                <div className="d-flex flex-column gap-3 justify-coontent-start">
+                <div className="sidebar-header-logo">
+                    <div className="logo-menu">
+                        {/* This is Sidebar layout responsivnes */}
+                        {collapsed ? (
+
+                            <i class="ri-menu-2-fill" onClick={() => setCollapsed(!collapsed)}></i>
+                        ) : (
+                            <i class="ri-close-line" onClick={() => setCollapsed(!collapsed)}></i>)}
+                    </div>
+                    <h1 className='logo'>EB</h1>
+                    <h1 className='role'>{user?.isAdmin ? 'Admin' : 'User'} :<br />{user?.name}</h1>
+                </div>
+                <div className="d-flex flex-column gap-3 justify-coontent-start menu">
                     {menuToBeRender.map((item, index) => {
                         return (
                             <div className={`${activeRoute === item.path && "active-menu-item"} menu-item`}>
                                 <i className={item.icon}></i>
-                                <span onClick={() => {
-                                    navigate(item.path);
-                                }}>{item.name}</span>
+                                {!collapsed && <span onClick={() => {
+                                    if (item.path == '/logout') {
+                                        localStorage.removeItem('token');
+                                        navigate('/login');
+                                    } else {
+                                        navigate(item.path);
+                                    }
+                                }}>{item.name}</span>}
                             </div>
                         );
                     })}
@@ -53,7 +91,13 @@ function DefaultLayout({ children }) {
             </div>
             <div className="body">
                 <div className="header">
-                    header
+                    {user && <h1>Welcome Back {user?.name} !</h1>}
+                 
+                    {/* {collapsed ? (
+                        <i class="ri-menu-2-fill" onClick={() => setCollapsed(!collapsed)}></i>
+                    ) : (
+                        <i class="ri-close-line" onClick={() => setCollapsed(!collapsed)}></i>)} */}
+
                 </div>
                 <div className="content">
                     {children}
