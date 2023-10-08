c0fc779e11abf0616dcf71158c95d0892b5d1772
BekCodingAddict <experte001@gmail.com>
2023-10-08 17:27:16 +0900

    Adding  Bus Form has created!

--- client/src/pages/Admin/AdminBuses.js
+++ client/src/pages/Admin/AdminBuses.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import PageTitle from '../../component/PageTitle';
+import BusForm from '../../component/BusForm';
 
 function AdminBuses(){
+    const [showBusForm, setShowBusForm]=React.useState(false);
     return (
-        <div>AdminBuses</div>
+        <div>
+            <div className='d-flex justify-content-between'>
+            <PageTitle title='Buses'/>
+            <button className='primary-btn'
+            onClick={()=>setShowBusForm(true)}
+            >Add Bus</button>
+            </div>
+
+            {showBusForm && <BusForm showBusForm={showBusForm} 
+            setShowBusForm={setShowBusForm}/>}
+        </div>
     )
 }
 
