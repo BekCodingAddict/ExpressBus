a85f2b40749aaf2a8a55aef5bf9acaced1f08d00
BekCodingAddict <experte001@gmail.com>
2023-10-15 18:53:07 +0900

    Displaying All User Buses page finished!

--- client/src/component/Bus.js
+++ client/src/component/Bus.js
@@ -1,19 +1,29 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
 
 function Bus({ bus }) {
+    const navigate=useNavigate();
     return (
         <div className="bus-card p-2 ">
             <h1 className="text-lg">{bus.name}</h1><hr />
-            <div className="d-flex justify-content-between">
+            <div className="d-flex justify-content-between gap-3" >
                 <div>
                     <p className="text-sm">From: {bus.from}</p>
+                </div>
+                <div>
+                    <p className="text-sm">To: {bus.to}</p>
                 </div>
                 <div>
                     <p className="text-sm">Price: ${bus.fare}/-</p>
                 </div>
                 <div>
-                <p className="text-sm">To: {bus.to}</p>
+                    <p className="text-sm">Journey Date: {bus.journeyDate}/-</p>
                 </div>
+            </div>
+            <div className="d-flex justify-content-between align-item-end">
+                <h1 className="text-lg underline" onClick={()=>{
+                    navigate(`/book-now/${bus._id}`)
+                }}>Book Now</h1>
             </div>
         </div>
 
