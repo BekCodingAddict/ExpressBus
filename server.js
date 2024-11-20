cf35c1ebdb5fa64c1f05b217673aeaac13101a96
unknown <experte001@gmail.com>
2024-11-21 01:08:04 +0900

    Preparing for Deployment

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ app.use("/api/users", usersRoute);
 app.use("/api/buses", busesRoute);
 app.use("/api/bookings", bookingsRoute);
 const path = require("path");
-if(process.env.NODE_ENV === "production")
-{
-    app.use(express.static("client/build"));
-  
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-    });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
 }
 
 app.listen(port, () => console.log(`Node server listening on port ${port}!`));
